5bd2b561c18e830d7c23351739fe8c17363f282c
Ayushvishwakarma04 <ayushvishwakarma9625@gmail.com>
2024-03-28 19:02:07 +0530

    First commit

---
+++ app.js
@@ -0,0 +1,57 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+//const request = require ("request");
+const app= express();
+const https = require("https");
+
+app.use(express.static("public")); //static to access the local files on server
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.get("/",function(req,res){
+    res.sendFile(__dirname+"/signup.html");
+});
+
+app.post("/",function(req,res){
+    const FirstName=req.body.Firstname;
+    const LastName=req.body.Lastname;
+    const Email=req.body.Email;
+    const data={
+        members: [{
+            email_address: Email,
+            status: "subscribed",
+            merge_fields: {
+                FNAME: FirstName,
+                LNAME: LastName 
+            }
+        }]
+    };
+    const jsondata=JSON.stringify(data)
+    const url = "https://us18.api.mailchimp.com/3.0/lists/888566541a";
+    const options = {
+        method: "POST",
+        auth: "Ayush4:8d39f09697b120241327f0376a2f7454-us18"
+    }
+    const request = https.request(url,options,function(response){
+        if (response.statusCode === 200){
+            res.sendFile(__dirname+"/success.html");
+        }else{
+            res.sendFile(__dirname+"/failure.html");
+        }
+        response.on("data",function(data){
+            console.log(JSON.parse(data));
+        });
+    })
+    request.write(jsondata);
+    request.end();
+})
+
+app.post("/failure",function(req,res){
+    res.redirect("/")
+})
+
+app.listen(process.env.PORT || 3000,function(){ //to run on heroku
+    console.log("Server running on 3000");
+});
+
+//8d39f09697b120241327f0376a2f7454-us18
+// List Id : 888566541a
